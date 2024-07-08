75cb53e6d4bc4baea238e5305b8e397292c67cc6
Evelyn <yalleyevelyn@gmail.com>
2024-07-08 16:42:48 -0700

    committing home work for recipe app

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,8 +1,41 @@
-export default function Recipes() {
+import RecipeCard from "../components/RecipeCard";
+import recipeOneImage from "../assets/images/recipe-one.png";
+import recipeTwoImage from "../assets/images/recipe-two.png";
+import recipeThreeImage from "../assets/images/recipe-three.png";
+import recipeFourImage from "../assets/images/recipe-four.png";
 
-    return(
-        <div>
-            Recipes
+export default function Recipes() {
+    return (
+        <div className="px-10 py-10">
+            
+            <div className="text-center font-bold"> <h3>Search</h3></div>
+
+            <div> 
+                <div><h3>What is in your kitchen</h3></div>
+                <p>Enter some ingredients</p>
+            </div>
+            <div>
+                <i>MG</i>
+                <input type="text" />
+            </div>
+
+            <div className="grid grid-cols-2 gap-5">
+                <RecipeCard
+                    image={recipeOneImage}
+                    name="Chorizo & mozzarella gnocchi bake" />
+                <RecipeCard
+                    image={recipeTwoImage}
+                    name="Coconut & squash curry" />
+                <RecipeCard
+                    image={recipeThreeImage}
+                    name="Huevos Rancheros" />
+                <RecipeCard
+                    image={recipeFourImage}
+                    name="Black forest Gateau" />
+            </div>
+
+
         </div>
-    )
+
+    );
 }
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome() {
     return(
         <div className="bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.5)),url('./assets/images/background_image.png')] bg-cover h-screen bg bg-center flex flex-col place-items-center place-content-center"> 
 
-            <h3 className="text-white text-center text-4xl ">Savory & started</h3>
+            <h3 className="text-white text-center text-4xl ">Savory & <br />Sweet</h3>
             <img src={welcomeImage} alt="welcome Image" />
         </div>
     )
